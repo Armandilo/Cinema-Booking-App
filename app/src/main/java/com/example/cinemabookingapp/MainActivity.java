0d7a3af7c22df9c29f29bd99bc985b5214e2258c
Armandilo <muhammadarmand@student.usm.my>
2022-01-06 02:43:03 +0800

    Added Login Page

--- app/src/main/java/com/example/cinemabookingapp/MainActivity.java
+++ app/src/main/java/com/example/cinemabookingapp/MainActivity.java
@@ -2,18 +2,37 @@ package com.example.cinemabookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                OpenLogin();
+            }
+        });
+
         try{
             this.getSupportActionBar().hide();
         }
         catch(NullPointerException e){}
+
     }
+    public void OpenLogin(){
+        Intent intent = new Intent(this,LoginPage.class);
+        startActivity(intent);
+    }
+
 }
