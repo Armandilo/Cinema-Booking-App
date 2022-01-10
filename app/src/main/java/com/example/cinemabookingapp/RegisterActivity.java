5b41874127f7851fb94fe14f6142e10a93626d45
behjat <behjatsolo123@gmail.com>
2022-01-10 17:26:22 +0800

    Project

---
+++ app/src/main/java/com/example/cinemabookingapp/RegisterActivity.java
@@ -0,0 +1,4 @@
+package com.example.cinemabookingapp;
+
+public class RegisterActivity {
+}
---
+++ app/src/main/java/com/example/cinemabookingapp/UserProfilePage.java
@@ -0,0 +1,8 @@
+package com.example.cinemabookingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserProfilePage extends AppCompatActivity {
+
+
+}
