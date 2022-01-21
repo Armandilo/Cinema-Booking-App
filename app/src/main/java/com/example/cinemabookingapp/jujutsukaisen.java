5d46df04b6a66de7370254ca1b9d14c898c69e8c
missnimz <nuruliffahmohdzukhi@gmail.com>
2022-01-22 00:17:06 +0800

    lagi dua trailer aku tak buat

---
+++ app/src/main/java/com/example/cinemabookingapp/jujutsukaisen.java
@@ -0,0 +1,14 @@
+package com.example.cinemabookingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class jujutsukaisen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jujutsukaisen);
+    }
+}
---
+++ app/src/main/java/com/example/cinemabookingapp/thebatman.java
@@ -0,0 +1,14 @@
+package com.example.cinemabookingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class thebatman extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thebatman);
+    }
+}
