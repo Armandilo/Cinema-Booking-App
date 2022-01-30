01bb1923da54b2354bbaa60afa035b9ccb8c00dc
behjat <behjatsolo123@gmail.com>
2022-01-30 16:47:47 +0800

    added navigation bar code

--- app/src/main/java/com/example/cinemabookingapp/UserProfilePage.java
+++ app/src/main/java/com/example/cinemabookingapp/UserProfilePage.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class UserProfilePage extends AppCompatActivity {
     private Button buttonlogout;
     private Button buttoneditprofile;
@@ -44,8 +46,29 @@ public class UserProfilePage extends AppCompatActivity {
             @Override
             public void onClick(View v) { OpenEditProfiler(); }
         });
+//Initialize and assign variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-
+        //Set Home Selected
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+//Perform ItemSelectedList
+        bottomNavigationView.setOnNavigationItemSelectedListener(menuItem -> {
+            switch(menuItem.getItemId()){
+                case R.id.nav_home:
+                    startActivity(new Intent(getApplicationContext(), HomePage.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                case R.id.nav_tickets:
+                    startActivity(new Intent(getApplicationContext(), Tickets.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                case R.id.nav_profile:
+                    startActivity(new Intent(getApplicationContext(), UserProfilePage.class));
+                    overridePendingTransition(0,0);
+                    return true;
+            }
+            return false;
+        });
 
         try{
             this.getSupportActionBar().hide();
